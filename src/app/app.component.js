b305b1f9a0a8b93ba0e7dc3a9f8856282c0fd772
rolland safort <tamchat_dev@163.com>
2017-04-09 21:11:01 +0800

    ready

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -8,8 +8,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require("@angular/core");
-var router_1 = require("@angular/router");
+var core_1 = require('@angular/core');
+var router_1 = require('@angular/router');
 var AppComponent = (function () {
     function AppComponent(router) {
         this.router = router;
@@ -31,14 +31,14 @@ var AppComponent = (function () {
         router.resetConfig(routes);
         console.log('route.config ----', JSON.stringify(router.config, undefined, 2));
     }
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            templateUrl: 'app/app.html'
+        }), 
+        __metadata('design:paramtypes', [router_1.Router])
+    ], AppComponent);
     return AppComponent;
 }());
-AppComponent = __decorate([
-    core_1.Component({
-        selector: 'my-app',
-        templateUrl: 'app/app.html'
-    }),
-    __metadata("design:paramtypes", [router_1.Router])
-], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
